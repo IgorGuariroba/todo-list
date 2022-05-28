06352d1621d90c72b1cbf05915d9ce6f9bb03106
igor.guariroba <igor.guariroba@jsl.com.br>
2022-05-28 17:24:07 -0300

    Desativar o delete enquanto é enviado para o servidor

--- vue3-composition-api/src/views/Todos/TodoList.vue
+++ vue3-composition-api/src/views/Todos/TodoList.vue
@@ -2,12 +2,13 @@
 <di>
 {{ todo.title}} - {{todo.body}}
   <router-link :to="{name: 'todos.edit', params: { id: todo.identify}}">Editar</router-link>
-  <a href="#" @click.prevent="deleteTodo">Deletar</a>
+  <a href="#" @click.prevent="deleteTodo" v-show="loading">Deletar</a>
 </di>
 </template>
 
 <script>
 import TodoService from '@/services/todos.services'
+import {ref} from "vue";
 export default {
   name: "TodoList",
   props: {
@@ -17,13 +18,17 @@ export default {
     }
   },
   setup(props,{emit}){
+    const loading = ref(true)
     const deleteTodo = () => {
+      loading.value = false
       TodoService.deleteTodo(props.todo.identify)
       .then(() => emit('todoDeleted', props.todo))
+      .finally(() =>  loading.value = true)
     }
 
     return {
-      deleteTodo
+      deleteTodo,
+      loading
     }
   }
 }
